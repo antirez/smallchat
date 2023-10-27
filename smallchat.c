d4348ee68147cd579242f50d876d7776d3ff6fa5
antirez <antirez@gmail.com>
2023-10-27 17:34:55 +0200

    Networking functions.

---
+++ smallchat.c
@@ -0,0 +1,95 @@
+/* smallchat.c -- Read clients input, send to all the other connected clients.
+ *
+ * Copyright (c) 2023, Salvatore Sanfilippo <antirez at gmail dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Redis nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+
+/* Set the specified socket in non-blocking mode, with no delay flag. */
+int socketSetNonBlockNoDelay(int fd) {
+    int flags, yes = 1;
+
+    /* Set the socket nonblocking.
+     * Note that fcntl(2) for F_GETFL and F_SETFL can't be
+     * interrupted by a signal. */
+    if ((flags = fcntl(fd, F_GETFL)) == -1) return -1;
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) return -1;
+
+    /* This is best-effort. No need to check for errors. */
+    setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes));
+    return 0;
+}
+
+/* Create a TCP socket lisetning to 'port' ready to accept connections. */
+int createTCPServer(int port) {
+    int s;
+    struct sockaddr_in sa;
+
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) return -1;
+
+    memset(&sa,0,sizeof(sa));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(s,&sa,sizeof(sa)) == -1 ||
+        listen(s, 511) == -1)
+    {
+        close(s);
+        return -1;
+    }
+    return s;
+}
+
+/* If the listening socket signaled there is a new connection ready to
+ * be accepted, we accept(2) it and return -1 on error or the new client
+ * socket on success. */
+int acceptClient(int server_socket) {
+    int s;
+
+    while(1) {
+        struct sockaddr_in sa;
+        s = accept(server_socket,&sa,sizeof(sa));
+        if (s == -1) {
+            if (errno == EINTR)
+                continue; /* Try again. */
+            else
+                return -1;
+        }
+        break;
+    }
+    return s;
+}
