6ee6e4e8e13c27c77e213366738d03dc2616783d
antirez <antirez@gmail.com>
2023-11-01 17:57:41 +0100

    Remove Redis name from cut&pasted license.

--- smallchat.c
+++ smallchat.c
@@ -11,7 +11,7 @@
  *   * Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of Redis nor the names of its contributors may be used
+ *   * Neither the project name of nor the names of its contributors may be used
  *     to endorse or promote products derived from this software without
  *     specific prior written permission.
  *
