f44382938b0bc12ceebd5701acfd80e9d336ea53
Subhankar1712 <140783367+Subhankar1712@users.noreply.github.com>
2023-11-02 12:07:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+# The configuration for the `remote` backend.
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-organization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+#       }
+#     }
