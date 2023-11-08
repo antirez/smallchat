08facc0441b9cddd94ce5153456fe1277c5dbe75
Marco De Paoli <depaolim@gmail.com>
2023-11-08 11:03:08 +0100

    refactoring OOP

--- smallchat.py
+++ smallchat.py
@@ -6,26 +6,74 @@ WELCOME = b"Welcome to Simple Chat! Use /nick <nick> to set your nick."
 PREFIX = b"/nick "
 
 
-class Clients:
+class Channel:
+    def __init__(self, conn, protocol_cls, notify_receive, notify_close):
+        self.conn = conn
+        self.notify_receive = notify_receive
+        self.notify_close = notify_close
+        self.fd = conn.fileno()
+        self.protocol = protocol_cls(self._received)
+        self.out_buffer = bytearray()
+
+    def raw_receive(self):
+        data = self.conn.recv(1024)
+        if not data:
+            self.notify_close(self)
+            self.conn.close()
+        else:
+            self.protocol.decode(data)
+
+    def raw_send(self):
+        if self.out_buffer:
+            sent = self.conn.send(self.out_buffer)
+            self.out_buffer = self.out_buffer[sent:]
+
+    def send(self, msg):
+        self.out_buffer += self.protocol.encode(msg)
+
+
+class Channels:
     def __init__(self, inputs, outputs):
         self.inputs = inputs
         self.outputs = outputs
         self.clients = {}
 
     def add(self, client):
-        print(f"Connected client fd={client.fd}, nick={client.nick}")
         self.clients[client.fd] = client
         self.inputs.append(client.conn)
         self.outputs.append(client.conn)
 
     def delete(self, client):
-        print(f"Disconnected client fd={client.fd}, nick={client.nick}")
         self.clients.pop(client.fd)
         self.inputs.remove(client.conn)
         self.outputs.remove(client.conn)
 
     def get(self, conn):
         return self.clients[conn.fileno()]
+
+
+class Client(Channel):
+    def __init__(self, conn, protocol_cls, publish, notify_close):
+        super().__init__(conn, protocol_cls, self._received, notify_close)
+        self.publish = publish
+        self.nick = f"user:{conn.fileno()}"
+
+    def _received(self, msg):
+        if msg.startswith(PREFIX):
+            self.nick = msg[len(PREFIX):].decode()
+        else:
+            self.publish(self, msg)
+
+
+class Clients(Channels):
+    def add(self, client):
+        super().add(client)
+        print(f"Connected client fd={client.fd}, nick={client.nick}")
+        client.send(WELCOME)
+
+    def delete(self, client):
+        super().delete(client)
+        print(f"Disconnected client fd={client.fd}, nick={client.nick}")
 
     def publish(self, sender, msg):
         response = sender.nick.encode() + b"> " + msg
@@ -52,39 +100,6 @@ class Protocol:
                 self.buff.append(car)
 
 
-class Client:
-    def __init__(self, conn, publish, notify_close):
-        self.conn = conn
-        self.publish = publish
-        self.notify_close = notify_close
-        self.fd = conn.fileno()
-        self.nick = f"user:{self.fd}"
-        self.protocol = Protocol(self._received)
-        self.out_buffer = bytearray()
-
-    def _received(self, msg):
-        if msg.startswith(PREFIX):
-            self.nick = msg[len(PREFIX):].decode()
-        else:
-            self.publish(self, msg)
-
-    def raw_receive(self):
-        data = self.conn.recv(1024)
-        if not data:
-            self.notify_close(self)
-            self.conn.close()
-        else:
-            self.protocol.decode(data)
-
-    def raw_send(self):
-        if self.out_buffer:
-            sent = self.conn.send(self.out_buffer)
-            self.out_buffer = self.out_buffer[sent:]
-
-    def send(self, msg):
-        self.out_buffer += Protocol.encode(msg)
-
-
 def main(host, port):
     address = (host, int(port))
     with socket.socket() as sl:
@@ -99,9 +114,8 @@ def main(host, port):
             for s in inputready:
                 if s == sl:
                     conn, addr = sl.accept()
-                    client = Client(conn, clients.publish, clients.delete)
+                    client = Client(conn, Protocol, clients.publish, clients.delete)
                     clients.add(client)
-                    client.send(WELCOME)
                 else:
                     client = clients.get(s)
                     assert client.conn == s
